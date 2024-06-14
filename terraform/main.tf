0e67c75695a48f7f9857102c5eb343746dfe88de
KinKinoV <mr.kolbasa.ask@gmail.com>
2024-06-14 10:53:14 +0300

    Formatted code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">=5.52.0"
     }
   }
 }
 
 provider "aws" {
-  region = var.region
+  region  = var.region
   profile = "default"
 }
 
