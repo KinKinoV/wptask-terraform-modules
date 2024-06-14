4067eedd780044d9a3b3f13d5daa9cfeb6bd205f
KinKinoV <mr.kolbasa.ask@gmail.com>
2024-06-14 10:46:21 +0300

    Changes due to moving project folder from Windows to WSL

--- terraform/010-network.tf
+++ terraform/010-network.tf
@@ -8,18 +8,21 @@ module "vpc" {
 
   name = "${var.environment}-wordpress-project"
   cidr = var.vpc_cidr
-  
+
   azs              = var.vpc_azs
-  public_subnets   = [cidrsubnet(var.vpc_cidr, 8, 1)]
-  private_subnets  = [for k, v in var.vpc_azs: cidrsubnet(var.vpc_cidr, 8, k+20)]
-  database_subnets = [for k, v in var.vpc_azs: cidrsubnet(var.vpc_cidr, 8, k+40)]
-  
-  enable_nat_gateway = false
+  public_subnets   = [for k, v in var.vpc_azs : cidrsubnet(var.vpc_cidr, 8, k)]
+  private_subnets  = [for k, v in var.vpc_azs : cidrsubnet(var.vpc_cidr, 8, k + 20)]
+  database_subnets = [for k, v in var.vpc_azs : cidrsubnet(var.vpc_cidr, 8, k + 40)]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
   create_database_subnet_group = true
-  
+
   enable_dns_hostnames = true
   enable_dns_support   = true
-  
+
   tags = local.tags
 }
 
@@ -28,61 +31,65 @@ module "vpc" {
 ###################
 
 module "bastion-sg" {
-  source = "terraform-aws-modules/security-group/aws"
+  source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
 
-  name = "${var.environment}-bastion-sg"
+  name        = "${var.environment}-bastion-sg"
   description = "Security Group for Bastion instance"
-  vpc_id = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = var.allowed_bastion_ips
-  ingress_rules = ["ssh-tcp"]
+  ingress_rules       = ["ssh-tcp"]
 
-  egress_cidr_blocks = [ "0.0.0.0/0" ]
-  egress_rules = [ "all-all" ]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
 
   tags = local.tags
 }
 
 module "wordpress-sg" {
-  source = "terraform-aws-modules/security-group/aws"
+  source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
 
-  name = "${var.environment}-wordpress-instances-sg"
+  name        = "${var.environment}-wordpress-instances-sg"
   description = "Security Group for WordPress instances in private network"
-  vpc_id = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
 
-  # Allow SSH connection from Bastion instance
-  ingress_cidr_blocks = [ "${module.bastion.private_ip}/32" ]
-  ingress_rules = [ "ssh-tcp" ]
+  ingress_with_cidr_blocks = [
+    # Allow SSH connection from Bastion instance
+    {
+      rule = "ssh-tcp"
+      cidr_blocks = "${module.bastion.private_ip}/32"
+    },
+   ]
 
   # Allows HTTP/HTTPS traffic from ALB
-  ingress_with_source_security_group_id = [ 
+  ingress_with_source_security_group_id = [
     {
-      rule = "http-80-tcp"
+      rule                     = "http-80-tcp"
       source_security_group_id = module.alb.security_group_id
     },
     {
-      rule = "https-443-tcp"
+      rule                     = "https-443-tcp"
       source_security_group_id = module.alb.security_group_id
     }
   ]
 
   egress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = [ "all-all" ]
+  egress_rules       = ["all-all"]
 
   tags = local.tags
 
-  depends_on = [ module.alb ]
+  depends_on = [module.alb]
 }
 
 module "db-sg" {
-  source = "terraform-aws-modules/security-group/aws"
+  source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
 
-  name = "${var.environment}-wordpress-database"
+  name        = "${var.environment}-wordpress-database"
   description = "Security Group for Multi-AZ RDS"
-  vpc_id = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
 
   ingress_with_cidr_blocks = [
     {
