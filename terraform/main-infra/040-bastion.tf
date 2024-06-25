480e112d1600a1206ba30a7699f25cf13f74c24e
KinKinoV <mr.kolbasa.ask@gmail.com>
2024-06-25 11:45:31 +0300

    Bastion now uses WordPress AMI

--- terraform/main-infra/040-bastion.tf
+++ terraform/main-infra/040-bastion.tf
@@ -1,17 +1,3 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners = [ "amazon" ]
-
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-  filter {
-    name = "architecture"
-    values = [ "x86_64" ]
-  }
-}
-
 resource "aws_key_pair" "bastion" {
   key_name   = "${var.environment}-wp-bastion"
   public_key = var.public_key_bastion
@@ -24,7 +10,7 @@ module "bastion" {
   name = "${var.environment}-bastion"
 
   instance_type               = "t3.micro"
-  ami                         = data.aws_ami.ubuntu.id
+  ami                         = var.wordpress_ami
   key_name                    = aws_key_pair.bastion.key_name
   subnet_id                   = element(module.vpc.public_subnets, 0)
   vpc_security_group_ids      = [module.bastion-sg.security_group_id]
