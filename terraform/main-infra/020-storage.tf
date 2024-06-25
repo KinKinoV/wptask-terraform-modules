965570429bc0dd63074c8cf65d6a024fb86fbde4
KinKinoV <mr.kolbasa.ask@gmail.com>
2024-06-25 11:44:49 +0300

    Now S3 bucket has policy to allow requests from CloudFront and w3tc-user that is needed for configuring proper offload of static assets

--- terraform/main-infra/020-storage.tf
+++ terraform/main-infra/020-storage.tf
@@ -2,12 +2,83 @@
 #                        S3 Buckets with required resources                    #
 ################################################################################
 
+# Policies for W3TC user
+data "aws_iam_policy" "s3-access" {
+  name = "AmazonS3FullAccess"
+}
+
+data "aws_iam_policy" "cloudfront-access" {
+  name = "CloudFrontReadOnlyAccess"
+}
+
+# W3TC IAM role
+module "w3tc-user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "5.39.1"
+  
+  name = "w3tc-access-user"
+
+  create_iam_user_login_profile = false
+  create_iam_access_key         = true
+  policy_arns                   = [data.aws_iam_policy.s3-access.arn, data.aws_iam_policy.cloudfront-access.arn]
+}
+
+# Bucket policy for W3TC IAM role
+data "aws_iam_policy_document" "w3tc-access" {
+  statement {
+    sid = "W3TCAccess"
+
+    principals {
+      type = "AWS"
+      identifiers = [ module.w3tc-user.iam_user_arn ]
+    }
+
+    actions = [ 
+      "s3:PutObject",
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.environment}-${var.bucket_name}",
+      "arn:aws:s3:::${var.environment}-${var.bucket_name}/*",
+    ]
+
+    effect = "Allow"
+  }
+
+  statement {
+    sid = "AllowCloudFrontServicePrincipal"
+
+    principals {
+      type = "Service"
+      identifiers = [ "cloudfront.amazonaws.com" ]
+    }
+
+    actions = [ 
+      "s3:GetObject",
+    ]
+
+    resources = [ 
+      "arn:aws:s3:::${var.environment}-${var.bucket_name}/*",
+    ]
+
+    condition {
+      test = "StringEquals"
+      variable = "AWS:SourceArn"
+      values = [ "${module.cloudfront.arn}" ]
+    }
+  }
+}
+
 module "s3-static" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.1.2"
 
   bucket        = "${var.environment}-${var.bucket_name}"
   force_destroy = true
+
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.w3tc-access
 
   tags = local.tags
 }
